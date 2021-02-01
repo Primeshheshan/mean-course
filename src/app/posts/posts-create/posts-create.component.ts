e387f0f29f90c6d5e468bc496a4b26f18231a0a6
primesh <primeshheshan@gmail.com>
2021-02-01 13:03:25 +0530

    added mat card , button and input area

--- src/app/posts/posts-create/posts-create.component.ts
+++ src/app/posts/posts-create/posts-create.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-create.component.css']
 })
 export class PostsCreateComponent implements OnInit {
-
+  inputValue ='';
+  newPost!: string;
   constructor() { }
 
+  onAddPost() {
+    this.newPost = this.inputValue;
+  }
   ngOnInit(): void {
   }
 
