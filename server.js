9fba46d5f421654f710587977fe2613bdfb03b5d
primesh <primeshheshan@gmail.com>
2021-02-02 15:23:47 +0530

    install express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require('http');
+const http = require('http'); // require is node.js import syntax
 
 const server = http.createServer((req, res) => {
   res.end("This is my first response");
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000); // lisining port:3000
