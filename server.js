6266c34de6ee570d6977edab39bd60f349f7b9c1
primesh <primeshheshan@gmail.com>
2021-02-02 15:38:46 +0530

    set first app in app.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http'); // require is node.js import syntax
+const app = require('./backend/app'); // import app
 
-const server = http.createServer((req, res) => {
-  res.end("This is my first response");
-});
+const port = process.env.PORT || 3000;
 
-server.listen(process.env.PORT || 3000); // lisining port:3000
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port); // lisining port:3000
