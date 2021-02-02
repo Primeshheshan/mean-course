3ed55bf0176fa4a9c5f51d56411a4c9ddbdf9b1b
primesh <primeshheshan@gmail.com>
2021-02-02 19:55:05 +0530

    send posts from app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
-const app = express();
+const app =  express();
 
-app.use((req, res, next) => {
-  console.log('First Middleware');
-  next();
-});
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
-  next();
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id: 'asd97a982s3', title: 'This is header 1', content: 'This is coming from server'},
+    {id: '52s15dsd5aw', title: 'This is header 2', content: 'This is coming from server'},
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succefully',
+    posts: posts
+  });
 });
 
 module.exports = app;
