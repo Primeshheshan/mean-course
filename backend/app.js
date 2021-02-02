87d186a86bc339f136e19e2c6d4175a01164d7aa
primesh <primeshheshan@gmail.com>
2021-02-02 22:18:01 +0530

    added post api end point

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const body = require('body-parser');
 
 const app =  express();
+
+app.use(bodyParser.json()); // parsing json data
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -9,13 +12,21 @@ app.use((req, res, next) => {
   next();
 });
 
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+})
+
 app.use('/api/posts', (req, res, next) => {
   const posts = [
     {id: 'asd97a982s3', title: 'This is header 1', content: 'This is coming from server'},
     {id: '52s15dsd5aw', title: 'This is header 2', content: 'This is coming from server'},
   ];
   res.status(200).json({
-    message: 'Posts fetched succefully',
+    message: 'Posts fetched successfully',
     posts: posts
   });
 });
