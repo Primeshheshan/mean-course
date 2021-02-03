b8398d3ca2bed0fb606571ee444b2593663403d1
primesh <primeshheshan@gmail.com>
2021-02-03 19:35:34 +0530

    set router paths to toolbar and sidenav and styled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
+
+const routes: Routes = [
+  { path: '', component: PostListComponent}, // root page
+  { path: 'create', component: PostsCreateComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
