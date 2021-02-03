e19dd5f8db4d1b303b2249144c40a72037ed924b
primesh <primeshheshan@gmail.com>
2021-02-03 10:45:12 +0530

    import post model to app.js and write post endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Post = require('./models/post');
+
 const app =  express();
 
 app.use(bodyParser.json()); // parsing json data
@@ -13,7 +15,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
@@ -22,8 +27,8 @@ app.post("/api/posts", (req, res, next) => {
 
 app.get('/api/posts', (req, res, next) => {
   const posts = [
-    {id: 'asd97a982s3', title: 'This is header 1', content: 'This is coming from server'},
-    {id: '52s15dsd5aw', title: 'This is header 2', content: 'This is coming from server'},
+    {id: 'asd97a982s3', title: 'This is server-side header 1', content: 'This is coming from server'},
+    {id: '52s15dsd5aw', title: 'This is server-side header 2', content: 'This is coming from server'},
   ];
   res.status(200).json({
     message: 'Posts fetched successfully',
