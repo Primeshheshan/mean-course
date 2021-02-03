37ae8ce254e3a9b325817612640d80da1f4b57e9
primesh <primeshheshan@gmail.com>
2021-02-03 13:02:24 +0530

    add delete one post method to app.js file

--- backend/app.js
+++ backend/app.js
@@ -48,8 +48,10 @@ app.get('/api/posts', (req, res, next) => {
 
 // delete a post
 app.delete('/api/posts/:id', (req, res, next) => {
-  console.log(req.params.id);
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
   res.status(200).json({message: "Post deleted successfully!"});
+  });
 
 });
 
