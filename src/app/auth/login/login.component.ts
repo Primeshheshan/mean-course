02615be1d32864e5388c14f9df7605324e5eb170
primesh <primeshheshan@gmail.com>
2021-02-07 15:24:46 +0530

    adding login.service and implement onlogin method

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +10,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   public loginFrom!: FormGroup;
   isLoading = false;
-  constructor() { }
+
+  constructor(public authService: AuthService) { }
 
   onLogin() {
-    console.log(this.loginFrom.value);
+    if(this.loginFrom.invalid) {
+      return;
+    }
+    this.authService.loginUser(this.loginFrom.value.email, this.loginFrom.value.password);
   }
 
   ngOnInit(): void {
     this.loginFrom = new FormGroup({
       email: new FormControl(null, {validators: [Validators.required, Validators.email] }),
-      password: new FormControl(null, Validators.required)
+      password: new FormControl(null, { validators: [Validators.required, Validators.minLength(6)]})
     });
   }
 
