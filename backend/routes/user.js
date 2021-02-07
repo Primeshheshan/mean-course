7a8a8cd5d1f7d3427c4784f2d32f9b1a914cb884
primesh <primeshheshan@gmail.com>
2021-02-07 15:25:59 +0530

    add login endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt =require("jsonwebtoken");
+const user = require("../models/user");
 
 
 const router = express.Router();
@@ -25,6 +27,45 @@ router.post("/signup", (req, res, next) => {
         });
       });
   });
+});
+
+// login endpoint
+router.post("/login", (req, res, next) => {
+  //find instance of that store in database
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+       //if pass above if block then user have valid password
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600
+      });
+    })
+    .catch(err => {
+      co
+      return res.status(401).json({
+        message: "Auth failed"
+      });
+    });
 });
 
 module.exports = router;
