5167a344a48a3e5dab0fda67ed374425b486e7fe
primesh <primeshheshan@gmail.com>
2021-02-08 12:37:01 +0530

    added space after to bearer string

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -11,7 +11,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authToken = this.authService.getToken();
     const authRequset = req.clone({
-      headers: req.headers.set('Authorization',"Bearer" + authToken)
+      headers: req.headers.set('Authorization',"Bearer " + authToken)
     });
     return next.handle(authRequset); // adding aour token and return
   }
