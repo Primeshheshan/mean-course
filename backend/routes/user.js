b55111aa25e8c6af73bcf40063e3fd790367ca17
primesh <primeshheshan@gmail.com>
2021-02-08 11:17:52 +0530

    added token to user.js file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const jwt =require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const user = require("../models/user");
 
 
