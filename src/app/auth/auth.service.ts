a9c981fc6e7e7b13300e0821b732d3fe3d11fd24
primesh <primeshheshan@gmail.com>
2021-02-08 12:37:59 +0530

    get token from login requst and return it

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { AuthData } from "./auth-data";
 
 @Injectable({providedIn: "root"})
 export class AuthService {
+  private token!: string;
 
   constructor(private http: HttpClient) {}
 
@@ -18,9 +19,14 @@ export class AuthService {
 
   loginUser(email: string, password: string) {
     const authData: AuthData = {email:email, password: password};
-    this.http.post("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
-        console.log(response);
+        const token = response.token;
+        this.token = token;
       });
+  }
+
+  getToken() {
+    return this.token;
   }
 }
