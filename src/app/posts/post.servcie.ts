0e7649334fab32fefac6f29f7f0dcf37d963ecdd
primesh <primeshheshan@gmail.com>
2021-02-10 21:02:45 +0530

    changed the methods postions and implement snackbar

--- src/app/posts/post.servcie.ts
+++ src/app/posts/post.servcie.ts
@@ -1,19 +1,74 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
+import { SnackBarComponent } from "../auth/snack-bar/snack-bar.component";
 import { PostModel } from "./post.model";
 
 @Injectable({providedIn: 'root'})
 export class PostServcie {
-  // subject getting back js object with posts, postCount property
-  private postUpdated = new Subject<{posts: PostModel[], postCount: number}>();
+  private postUpdated = new Subject<{posts: PostModel[], postCount: number}>(); // subject getting back js object with posts, postCount property
   private posts: PostModel[]= []; // initally empty array
 
   constructor(
     private http: HttpClient,
-    private router: Router) {}
+    private router: Router,
+    private _snackBar: MatSnackBar) {}
+
+  // add new post
+  addPosts(title:string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    postData.append("image", image, title);
+    this.http.post<{message: string, post: PostModel}>("http://localhost:3000/api/posts", postData)
+      .subscribe( responseData => {
+        this.router.navigate(["/"]);
+        this.openSnackBar("Post created successfully", "snackbar-success");
+      });
+  }
+
+  //delete post
+  deletePost(postId: string) {
+    return this.http.delete("http://localhost:3000/api/posts/" + postId);
+  }
+
+  // update post
+  updatePost(postId: string, title: string, content: string, image: File | string) {
+    let postData: PostModel | FormData;
+    if(typeof(image) === 'object') {
+       postData = new FormData();
+      postData.append("id", postId); // FormData objects
+      postData.append("title", title); // FormData objects
+      postData.append("content", content); // FormData objects
+      postData.append("image", image, title); // FormData objects
+    } else {
+       postData = {
+        id: postId,
+        title: title,
+        content: content,
+        imagePath: image,
+        creator: ""
+      }
+    }
+    // this is updated post element
+    this.http.put("http://localhost:3000/api/posts/" + postId, postData)
+    .subscribe(response => {
+      this.router.navigate(["/"]);
+      this.openSnackBar("Post updated successfully", "snackbar-success");
+    });
+  }
+
+  openSnackBar(data: string, style: string) {
+    const styleClass = style;
+    this._snackBar.openFromComponent(SnackBarComponent, {
+      duration: 4000,
+      data: data,
+      panelClass: [styleClass]
+    });
+  }
 
   getPosts(postsPerPage: number, currentPage: number) {
     // return [...this.posts]; not return original array return copy of array
@@ -55,50 +110,7 @@ export class PostServcie {
     ("http://localhost:3000/api/posts/" + id);
   }
 
-  // add new post
-  addPosts(title:string, content: string, image: File) {
-    const postData = new FormData();
-    postData.append("title", title);
-    postData.append("content", content);
-    postData.append("image", image, title);
 
-    this.http.post<{message: string, post: PostModel}>("http://localhost:3000/api/posts", postData)
-      .subscribe( responseData => {
-
-        this.router.navigate(["/"]);
-      });
-
-  }
-
-  //delete post
-  deletePost(postId: string) {
-    return this.http.delete("http://localhost:3000/api/posts/" + postId);
-  }
-
-  // update post
-  updatePost(postId: string, title: string, content: string, image: File | string) {
-    let postData: PostModel | FormData;
-    if(typeof(image) === 'object') {
-       postData = new FormData();
-      postData.append("id", postId); // FormData objects
-      postData.append("title", title); // FormData objects
-      postData.append("content", content); // FormData objects
-      postData.append("image", image, title); // FormData objects
-    } else {
-       postData = {
-        id: postId,
-        title: title,
-        content: content,
-        imagePath: image,
-        creator: ""
-      }
-    }
-    // this is updated post element
-    this.http.put("http://localhost:3000/api/posts/" + postId, postData)
-    .subscribe(response => {
-      this.router.navigate(["/"]);
-    });
-  }
 
 
 }
