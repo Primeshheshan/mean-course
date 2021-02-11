67e043c58bd2028f842374de97591d66449306e3
primesh <primeshheshan@gmail.com>
2021-02-11 14:51:27 +0530

    created error interceptor to show errors

---
+++ src/app/error-interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { AuthService } from "./auth/auth.service";
+import { ErrorComponent } from "./error/error.component";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(
+    private authService: AuthService,
+    private dialog: MatDialog) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = "An unknown error occured!"
+        if(error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialog.open(ErrorComponent, {
+          width: '350px',
+          data: {message: errorMessage}});
+        return throwError(error);
+      })
+    ); // lisening response
+  }
+}
