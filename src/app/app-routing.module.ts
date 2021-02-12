3836b75c4c7ba62577e4422f2032801f20d6ee59
primesh <primeshheshan@gmail.com>
2021-02-12 21:11:13 +0530

    removed login and signup from app-routing.module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth/auth-guard';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 
+import { AuthGuard } from './auth/auth-guard';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent}, // root page
-  { path: 'login', component: LoginComponent},
-  { path: 'signup', component: SignupComponent},
+  { path: 'auth', loadChildren: () => import('./auth/auth.moudle').then(m => m.AuthModule)},
   { path: 'create', component: PostsCreateComponent, canActivate: [AuthGuard]},
   { path: 'edit/:postId', component: PostsCreateComponent, canActivate: [AuthGuard]},
 ];
