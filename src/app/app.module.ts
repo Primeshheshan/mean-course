33c1abfe7b1220f8294cc564535bf9088aea90ac
primesh <primeshheshan@gmail.com>
2021-02-12 21:12:30 +0530

    removed login and signup components from app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AngularMaterialmodule } from './angular-material.module'; //materials
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AutoFocusDirective } from './auto-focus.directive';
 import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { AutoFocusDirective } from './auto-focus.directive';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
+import { LogoutDialogComponent } from './main-nav/logout-dialog/logout-dialog.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { SnackBarComponent } from './auth/snack-bar/snack-bar.component';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
-import { LogoutDialogComponent } from './main-nav/logout-dialog/logout-dialog.component';
-import { AngularMaterialmodule } from './angular-material.module'; //materials
-
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -29,8 +27,6 @@ import { AngularMaterialmodule } from './angular-material.module'; //materials
     PostListComponent,
     MainNavComponent,
     AutoFocusDirective,
-    LoginComponent,
-    SignupComponent,
     SnackBarComponent,
     ErrorComponent,
     LogoutDialogComponent
@@ -44,7 +40,7 @@ import { AngularMaterialmodule } from './angular-material.module'; //materials
     HttpClientModule,
     LayoutModule,
     AngularMaterialmodule, //materials
-
+    AuthRoutingModule // login and signup routing
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
