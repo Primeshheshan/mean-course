a4391764225d3025fa4fe50c3b9f53dc07fd5f8e
primesh <primeshheshan@gmail.com>
2021-02-12 21:44:42 +0530

    created global variable for backend url

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,8 +3,11 @@ import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { Router } from "@angular/router";
 import { Subject } from "rxjs";
+import { environment } from "src/environments/environment";
 import { AuthData } from "./auth-data";
 import { SnackBarComponent } from "./snack-bar/snack-bar.component";
+
+const BACKEND_URL = environment.apiUrl + "/user/";
 
 @Injectable({providedIn: "root"})
 export class AuthService {
@@ -23,7 +26,7 @@ export class AuthService {
   createUser(email: string, password: string) {
     const authData: AuthData = {email:email, password: password};
     //send post request
-    return this.http.post("http://localhost:3000/api/user/signup", authData)
+    return this.http.post(BACKEND_URL + "signup", authData)
       .subscribe(result => {
         this.router.navigate(["/"]);
       }, error => {
@@ -34,7 +37,7 @@ export class AuthService {
 
   loginUser(email: string, password: string) {
     const authData: AuthData = {email:email, password: password};
-    this.http.post<{token: string, expiresIn: number, userId: string}>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number, userId: string}>(BACKEND_URL + "login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
