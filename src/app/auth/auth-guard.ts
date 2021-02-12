3db0c87052dfe37faed72fddbf0f9057ba3e6a17
primesh <primeshheshan@gmail.com>
2021-02-12 11:40:32 +0530

    add dialog error message to authguard

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
+import { ErrorComponent } from "../error/error.component";
 import { AuthService } from "./auth.service";
 import { SnackBarComponent } from "./snack-bar/snack-bar.component";
 
@@ -11,7 +13,8 @@ export class AuthGuard implements CanActivate{
   constructor(
     private authService: AuthService,
     private router: Router,
-    private _snackBar: MatSnackBar) {}
+    private _snackBar: MatSnackBar,
+    private dialog: MatDialog) {}
 
   canActivate( // return true - can navigate
     route: ActivatedRouteSnapshot, // route trying to load
@@ -20,7 +23,11 @@ export class AuthGuard implements CanActivate{
     const isAuth = this.authService.getIsAuth();
     if(!isAuth) { //  not authenticated
       this.router.navigate(["/login"]);
-      this.authService.openSnackBar("Not logged in, Please login or signup to continue.!","snackbar-error");
+      // this.authService.openSnackBar("Not logged in, Please login or signup to continue.!","snackbar-error");
+      this.dialog.open(ErrorComponent, {
+        width: '350px',
+        data: {message: "Not logged in, Please login or signup to continue.!"},
+        panelClass: "error-dialog"});
     }
     return isAuth;
   }
